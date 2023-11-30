717533943b2ac82e762d4e75b161ea0ef53350f0
luke-sonn <luke.son@goorm.io>
2023-11-30 23:37:25 +0900

    hi

--- krampoline/src/App.js
+++ krampoline/src/App.js
@@ -1,15 +1,20 @@
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
-
-import Main from './Main';
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    const sendPing = async () => {
+        try {
+            const { data } = await axios.get("0.0.0.0:8080/api/ping");
+            console.log("data: ", data);
+        } catch (error) {
+            console.log("error: ", error);
+        }
+    };
+    return (
+        <div className="App">
+            <button onClick={sendPing}>핑 날리기</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
